50d96a2b2c0abe96e805d781082a29ccea18fa4f
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2021-05-09 10:01:17 -0400

    first commit

---
+++ src/main/java/lms/LmsSaApplication.java
@@ -0,0 +1,13 @@
+package lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lms/LmsSaApplicationTests.java
@@ -0,0 +1,13 @@
+package lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
