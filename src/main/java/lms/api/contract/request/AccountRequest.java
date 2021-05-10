05ee81aabe69d2d6d78cb4aee8e515dc7ce22ffa
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2021-05-10 00:47:58 -0400

    fix

--- src/main/java/lms/api/contract/request/AccountRequest.java
+++ src/main/java/lms/api/contract/request/AccountRequest.java
@@ -2,6 +2,7 @@ package lms.api.contract.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class AccountRequest {
 	@NotBlank
 	private String name;
 
+	@NotEmpty
 	@Email
 	private String email;
 
